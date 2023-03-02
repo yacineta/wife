064a2624f9e2dbb045c13d44a7661c1bdc732271
yacineta <120061436+yacineta@users.noreply.github.com>
2023-03-02 17:40:37 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+let title = document.querySelector(".titel");
+let ul = document.querySelector("ul");
+let reload = document.querySelector(".reload");
+window.onload = function(){
+    if(window.navigator.onLine){
+        onLine()
+    } else{
+        offLine()
+    }
+    
+
+}
+window.addEventListener("online",function(){
+    online();
+});
+window.addEventListener("offline",function(){
+    offLine();
+});
+
+reload.onclick = function(){
+    window.location.reload();
+}
+
+function onLine(){
+    title.innerHTML = 'Online Now';
+    title.style.color = 'green';
+    ul.classList.add('hide');
+    reload.classList.add('hide');
+
+}
+function offLine(){
+    title.innerHTML = 'Offline Now';
+    title.style.color = '#666';
+    ul.classList.remove('hide');
+    reload.classList.remove('hide');
+
+}
